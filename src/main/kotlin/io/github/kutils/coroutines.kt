65f204621001d99717f9cdff948e1e78495e3891
Attila Turcsan <mail@attilaturcsan.hu>
2018-11-15 14:36:46 +0100

    update 1.3.0 kotlin, 1.0.0 stable coroutine

--- src/main/kotlin/io/github/kutils/coroutines.kt
+++ src/main/kotlin/io/github/kutils/coroutines.kt
@@ -1,26 +1,35 @@
 package io.github.kutils
 
-import kotlinx.coroutines.experimental.CommonPool
+import kotlinx.coroutines.asCoroutineDispatcher
 import java.util.concurrent.CompletableFuture
-import kotlin.coroutines.experimental.Continuation
-import kotlin.coroutines.experimental.CoroutineContext
-import kotlin.coroutines.experimental.startCoroutine
-import kotlin.coroutines.experimental.suspendCoroutine
+import java.util.concurrent.ForkJoinPool
+import kotlin.coroutines.Continuation
+import kotlin.coroutines.CoroutineContext
+import kotlin.coroutines.resume
+import kotlin.coroutines.resumeWithException
+import kotlin.coroutines.startCoroutine
+import kotlin.coroutines.suspendCoroutine
 
 /**
  * @author atistrcsn - 2017
  */
 
-fun <T> future(context: CoroutineContext = CommonPool, block: suspend () -> T): CompletableFuture<T> =
-        CompletableFutureCoroutine<T>(context).also { block.startCoroutine(completion = it) }
+fun <T> future(context: CoroutineContext = ForkJoinPool.commonPool().asCoroutineDispatcher(), block: suspend () -> T): CompletableFuture<T> =
+        CompletableFutureCoroutine<T>(context)
+                .also {
+                    block.startCoroutine(completion = it)
+                }
 
 class CompletableFutureCoroutine<T>(override val context: CoroutineContext) : CompletableFuture<T>(), Continuation<T> {
-    override fun resume(value: T) {
-        complete(value)
-    }
-
-    override fun resumeWithException(exception: Throwable) {
-        completeExceptionally(exception)
+    override fun resumeWith(result: Result<T>) {
+        if (result.isSuccess)
+            result.getOrNull()?.let {
+                complete(it)
+            }
+        else
+            result.exceptionOrNull()?.let {
+                completeExceptionally(it)
+            }
     }
 }
 
